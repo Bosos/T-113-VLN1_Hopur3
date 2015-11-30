85d2c6af151f1d290694133dc5742e4278785eb7
Bjarturfannar <bjartur15@ru.is>
2015-11-30 22:13:03 +0000

    Text added
    var að adda texta a sort

--- ComputerScientistsW1/datamanager.cpp
+++ ComputerScientistsW1/datamanager.cpp
@@ -84,6 +84,7 @@ vector<Scientist> DataManager::findByDeathYear(string year,SortBy sort, Directio
     return vector<Scientist>();
 }
 
+// Calculates age of scientist.
 int DataManager::getage(Scientist oneScientist){
 
     if(oneScientist.getDeathYear() == 0){
@@ -93,7 +94,7 @@ int DataManager::getage(Scientist oneScientist){
    return oneScientist.getDeathYear() - oneScientist.getBirthYear();
 }
 
-//skilar vector af Scientist, tekur inn vector af Scientist
+
 
 struct sortByName
 {
@@ -103,6 +104,7 @@ struct sortByName
     }
 };
 
+
 struct sortByBirth
 {
     inline bool operator() (Scientist& birth1, Scientist& birth2)
@@ -110,6 +112,7 @@ struct sortByBirth
         return (birth1.getBirthYear() < birth2.getBirthYear());
     }
 };
+
 
 struct sortByDeath
 {
@@ -119,6 +122,7 @@ struct sortByDeath
     }
 };
 
+
 struct sortBySex
 {
     inline bool operator() (Scientist& sex1, Scientist& sex2)
@@ -127,6 +131,8 @@ struct sortBySex
     }
 };
 
+
+// This function gets an input from the user and sorts it.
 vector<Scientist> DataManager::sortBy(vector<Scientist> scientists, SortBy sortBy, Direction direction )
 {
     switch(sortBy)
@@ -134,18 +140,22 @@ vector<Scientist> DataManager::sortBy(vector<Scientist> scientists, SortBy sortB
         case NONE:
             return scientists;
 
+        // Sorts names in alphabetical order.
         case NAME:
             sort(scientists.begin(), scientists.end(), sortByName());
             break;
 
+        // Sorts birthyear "lowest to highest".
         case BIRTH:
             sort(scientists.begin(), scientists.end(), sortByBirth());
             break;
 
+        // Sorts deathyear "lowest to highest".
         case DEATH:
             sort(scientists.begin(), scientists.end(), sortByDeath());
             break;
 
+        // Sorts gender.
         case SEX:
             sort(scientists.begin(), scientists.end(), sortBySex());
             break;
@@ -154,7 +164,7 @@ vector<Scientist> DataManager::sortBy(vector<Scientist> scientists, SortBy sortB
             return scientists;
 
     }
-
+// Reverses the elements in the Scientists vector.
     if(direction == DESCENDING){
         reverse (scientists.begin(), scientists.end());
     }
